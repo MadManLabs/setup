ff382a605281d83e3ba4479af28b58d0b79ebbb8
hstrausl@outwardinc.com <hstrausl@outwardinc.com>
2017-08-13 15:13:25 -0700

    added colors

--- config.cmd
+++ config.cmd
@@ -1,6 +1,13 @@
 :: Hans Strausl's Personal config.cmd to run when opening cmd.exe
 @echo off
 
+set /a rand1=%random% %% 11
+set /a rand2=%random% %% 2
+set HEX=234578BCDEF
+set HEX2=0F
+call set hexcolors=%%HEX:~%rand1%,1%%%%HEX2:~%rand2%,1%%
+color %hexcolors%
+title %hexcolors%
 set PROMPT=$P$_$G$S
 
 set DIRCMD=/W
@@ -19,10 +26,13 @@ doskey d = cd %HOMEDRIVE%%HOMEPATH%\DESKTOP
 doskey exp = explorer.exe .
 
 :: Needs bash on win10 enabled
-doskey vim = bash -c "vim $*"
+doskey vim = bash -c "vim $(echo '$*' | sed 's/\\/\//g')"
+doskey ssh = bash -c "ssh $*"
 
 :: doskey hg = hg branch $G %SYSTEMROOT%\branch.txt $T set /p HGB= $L %SYSTEMROOT%\branch.txt $T hg $*
 doskey hg = echo off $T FOR /F "delims=" %%i IN ('hg branch') DO set HGB=%%i $G nul $T hg $* $T echo on
 doskey git = echo off $T FOR /F "tokens=2" %%i IN ('git branch') DO set GITB=%%i $G nul $T git $* $T echo on
 
 doskey grep = bash -c "grep $*"
+
+doskey ex = node -p "$*"
